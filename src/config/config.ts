ab35161b97dec86e0aa03465e12d6b86940d62da
Miguel Mayans <miguelmayansv@gmail.com>
2023-12-21 23:15:18 +0100

    try: fix vercel delete mongo 9

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,64 +1,61 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 type TConfig = {
-    [key: string]: EnviromentConfig
-}
+  [key: string]: EnviromentConfig;
+};
 
 type EnviromentConfig = {
-    app: AppConfig
-    db: MongoDBConfig
-    auth0?: AUTH0
-}
+  app: AppConfig;
+  db?: MongoDBConfig;
+  auth0?: AUTH0;
+};
 
 type MongoDBConfig = {
-    URI: string
-}
+  URI: string;
+};
 
 type AppConfig = {
-    PORT: string | number
-}
+  PORT: string | number;
+  ORIGIN: string | undefined;
+};
 
 type AUTH0 = {
-    client_origin: string | undefined
-    audience: string | undefined
-    issuer: string | undefined
-}
+  client_origin: string | undefined;
+  audience: string | undefined;
+  issuer: string | undefined;
+};
 
 if (process.env.NODE_ENV === "production") {
-
-    dotenv.config({ path: ".env.production" })
-
+  dotenv.config({ path: ".env.production" });
 } else {
-
-    dotenv.config({ path: ".env.development" })
+  dotenv.config({ path: ".env.development" });
 }
 
-const ENV = process.env.NODE_ENV ?? "development"
+const ENV = process.env.NODE_ENV ?? "development";
 
 const CONFIG: TConfig = {
-    development: {
-        app: {
-            PORT: process.env.PORT || 4001
-        },
-        db: {
-            URI:
-                process.env.MONGO_DB_URI || "mongodb://localhost:27017/test_development"
-        },
-        auth0: {
-            client_origin: process.env.APP_ORIGIN,
-            audience: process.env.AUTH0_AUDIENCE,
-            issuer: process.env.AUTH0_ISSUER
-        }
+  development: {
+    app: {
+      PORT: process.env.PORT || 4001,
+      ORIGIN: process.env.APP_ORIGIN,
     },
-    production: {
-        app: {
-            PORT: process.env.PORT || 4002
-        },
-        db: {
-            URI:
-                process.env.MONGO_DB_URI || "mongodb://localhost:27017/test_production"
-        }
-    }
-}
+    auth0: {
+      client_origin: process.env.APP_ORIGIN,
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: process.env.AUTH0_ISSUER,
+    },
+  },
+  production: {
+    app: {
+      PORT: process.env.PORT || 4001,
+      ORIGIN: process.env.APP_ORIGIN,
+    },
+    auth0: {
+      client_origin: process.env.APP_ORIGIN,
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: process.env.AUTH0_ISSUER,
+    },
+  },
+};
 
-export default CONFIG[ENV]
+export default CONFIG[ENV];
