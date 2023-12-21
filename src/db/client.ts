aeb19f6f987b9e1019bc52724bd98cac122c4d66
Miguel Mayans <miguelmayansv@gmail.com>
2023-12-21 15:26:46 +0100

    try: fix vercel bug10

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,19 +2,21 @@ import { PrismaClient as MongoClient } from "../../prisma/generated/mongodb_clie
 import { PrismaClient as PostgresClient } from "../../prisma/generated/postgresql_client";
 import { Prisma } from "@prisma/client";
 import { DefaultArgs } from "@prisma/client/runtime/library";
+import { PrismaClientOptions } from "../../prisma/generated/mongodb_client/runtime/library";
+import { PrismaClientOptions as PosOptions } from "../../prisma/generated/postgresql_client/runtime/library";
 
-export const DATA_SOURCE = process.env.DATA_SOURCE ?? "mongo"
+export const DATA_SOURCE = process.env.DATA_SOURCE ?? "mongo";
 
-type ClientMongo = MongoClient<Prisma.PrismaClientOptions, never, DefaultArgs>
-type ClientPostgres = PostgresClient<Prisma.PrismaClientOptions, never, DefaultArgs>
+type ClientMongo = MongoClient<PrismaClientOptions, never, DefaultArgs>;
+type ClientPostgres = PostgresClient<PosOptions, never, DefaultArgs>;
 
 export const mongoClient: ClientMongo = new MongoClient();
 export const postgresClient: ClientPostgres = new PostgresClient();
 
-export let prismaClient: any
+export let prismaClient: any;
 
 if (DATA_SOURCE === "postgres") {
-    prismaClient = postgresClient
+  prismaClient = postgresClient;
 } else {
-    prismaClient = mongoClient
+  prismaClient = mongoClient;
 }
